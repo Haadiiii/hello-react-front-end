1d15fb3684d66790556410e60540ab81c6eb0000
Haadiiii <hammali99@gmail.com>
2023-01-03 22:01:11 +0500

    Create app and initialize req

---
+++ src/redux/reducer.js
@@ -0,0 +1,34 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const GREETINGS = "GREETINGS";
+
+const init = [];
+
+const greetingReducer = (state = init, action) => {
+  switch (action.type) {
+    case GREETINGS:
+      return action.greetings
+    default:
+      return state;
+  }
+};
+
+export default greetingReducer;
+
+const url = "http://localhost:3001/api/v1/greetings";
+
+export const getGreetings = createAsyncThunk( GREETINGS, async (args, {dispatch}) => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }
+  try {
+    const {data} = await axios.get(url, config);
+    dispatch({ type: GREETINGS, greetings: data[0] });
+    console.log("data");
+  } catch (err) {
+    console.log(err);
+  }
+});
